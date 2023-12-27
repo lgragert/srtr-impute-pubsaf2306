bfb71a92f6ec8a47aa850c0ba543a652e70e3dde
Loren Gragert <lgragert@tulane.edu>
2023-12-27 14:29:11 -0600

    Allele name fix

--- aa_mm_biopython_runmatch_genie_9loc.py
+++ aa_mm_biopython_runmatch_genie_9loc.py
@@ -737,20 +737,20 @@ for rep in tqdm(range(1,multiple_imputation_replicates+1)):
 				# AA = getAAposition(HLA_seq,allele1_donor,pos)
 				# print (allele1_donor + " - " + str(pos) + AA)
 				mm_count = 0
-				if (allele1_recip[pos] == 'DRBX*NNNN' and allele2_recip[pos] == 'DRBX*NNNN'):
+				if (allele1_recip == 'DRBX*NNNN' and allele2_recip == 'DRBX*NNNN'):
 					mm_count == 2
-				elif (allele1_donor[pos] == 'DRBX*NNNN' and allele2_donor[pos] == 'DRBX*NNNN' ):
+				elif (allele1_donor == 'DRBX*NNNN' and allele2_donor == 'DRBX*NNNN' ):
 					mm_count == 2
 				else:
-					if (allele1_donor[pos]  == 'DRBX*NNNN'):
-						allele1_donor[pos]  = allele2_donor[pos] 
-					if (allele2_donor[pos]  == 'DRBX*NNNN'):
-						allele2_donor[pos]  = allele1_donor[pos] 
-					if (allele1_recip[pos] == 'DRBX*NNNN'):
-						allele1_recip[pos] = allele2_recip[pos] 
-					if (allele2_recip[pos]  == 'DRBX*NNNN'):
-						allele2_recip[pos]  = allele1_recip[pos]
-					mm_count = genie.countAAMismatchesAllele(allele1_donor[pos],allele2_donor[pos],allele1_recip[pos],allele2_recip[pos],pos)
+					if (allele1_donor  == 'DRBX*NNNN'):
+						allele1_donor  = allele2_donor
+					if (allele2_donor  == 'DRBX*NNNN'):
+						allele2_donor  = allele1_donor
+					if (allele1_recip == 'DRBX*NNNN'):
+						allele1_recip = allele2_recip
+					if (allele2_recip  == 'DRBX*NNNN'):
+						allele2_recip  = allele1_recip
+					mm_count = genie.countAAMismatchesAllele(allele1_donor,allele2_donor,allele1_recip,allele2_recip,pos)
 				#print ("MM_count at position " + str(pos) + ": " + str(mm_count) + " - HLA - " + allele1_donor + " " + allele2_donor + " " + allele1_recip + " " + allele2_recip)
 
 				mm_count_0 = 0
