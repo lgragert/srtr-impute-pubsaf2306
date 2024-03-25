404c9ff8c6dae32c41e09921ec29e89d9b582627
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-03-25 08:41:31 -0500

    Loci Grouping in Genomic Order

--- AA_TRS_Manhattan_Plot.R
+++ AA_TRS_Manhattan_Plot.R
@@ -47,11 +47,23 @@ nam_donor = trs_NAM %>% filter((Subject_Type =='DONOR') & (Ethnicity == 'NAM'))
 
 # GGplot function found on https://r-graph-gallery.com/101_Manhattan_plot.html
 manhattan_plot <- function(sub_typ, ethnicity, subj, tolerance=0.075) {
+  # Make Loci names L1, L2, L3,... so that you can have it in genomic order
+  sub_typ$lgroup <- 'NA'
+  sub_typ[is.element(sub_typ$Locus,'A'),"lgroup"] <- 'L1'
+  sub_typ[is.element(sub_typ$Locus,'C'),"lgroup"] <- 'L2'
+  sub_typ[is.element(sub_typ$Locus,'B'),"lgroup"] <- 'L3'
+  sub_typ[is.element(sub_typ$Locus,'DRB345'),"lgroup"] <- 'L4'
+  sub_typ[is.element(sub_typ$Locus,'DRB1'),"lgroup"] <- 'L5'
+  sub_typ[is.element(sub_typ$Locus,'DQA1'),"lgroup"] <- 'L6'
+  sub_typ[is.element(sub_typ$Locus,'DQB1'),"lgroup"] <- 'L7'
+  sub_typ[is.element(sub_typ$Locus,'DPA1'),"lgroup"] <- 'L8'
+  sub_typ[is.element(sub_typ$Locus,'DPB1'),"lgroup"] <- 'L9'
+  
   # Prepare the data
   don <- sub_typ %>%
     
     # Compute locus size by AA position
-    group_by(Locus) %>%
+    group_by(lgroup) %>%
     summarise(chr_len=max(AA_Position)) %>%
     
     # Calculate cumulative position of each loci
@@ -59,14 +71,14 @@ manhattan_plot <- function(sub_typ, ethnicity, subj, tolerance=0.075) {
     select(-chr_len) %>%
     
     # Add this info to the initial dataset
-    left_join(sub_typ, ., by=c('Locus'='Locus')) %>%
+    left_join(sub_typ, ., by=c('lgroup'='lgroup')) %>%
     
     # Add a cumulative position of each AA position
-    arrange(Locus, AA_Position) %>%
+    arrange(lgroup, AA_Position) %>%
     mutate(Loci=AA_Position+tot) %>%
     
     # Add highlight to big variance in TRS Average
-    mutate( is_annotate=ifelse((1- TRS_Average)>tolerance, "yes", "no"))
+    mutate(is_annotate=ifelse((1- TRS_Average)>tolerance, "yes", "no"))
   
   # Make the x-axes
   axisdf = don %>%
@@ -79,11 +91,11 @@ manhattan_plot <- function(sub_typ, ethnicity, subj, tolerance=0.075) {
   ggplot(don, aes(x=Loci, y=(1-TRS_Average))) +
     
     # Show all points
-    geom_point( aes(color=as.factor(Locus)), alpha=0.8, size=1.3) +
-    scale_color_manual(values = rep(c("blue", "red"), 22 )) +
+    geom_point(aes(color=as.factor(lgroup)), alpha=0.8, size=1.3) +
+    scale_color_manual(values=rep(c("blue", "red"), 22 )) +
     
     # custom X axis:
-    scale_x_continuous(label = axisdf$Locus, breaks= axisdf$center ) +
+    scale_x_continuous(label=axisdf$Locus, breaks=axisdf$center ) +
     scale_y_continuous(expand = c(0, 0), limits=c(0, max_y_val + 0.01) ) +     # remove space between plot area and x axis
     
     # Add label using ggrepel to avoid overlapping
