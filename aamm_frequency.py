9e3887c39d6e258ec24e22d82e0088d8492c1ca0
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-04-10 09:07:21 -0500

    Created more CSV files

--- aamm_frequency.py
+++ aamm_frequency.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from pathlib import Path
 import pandas as pd
 from collections import defaultdict
 import gzip
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # Count the MMs and find the frequencies for overall MM, 1 MM count, and 2 MM count
 def aamm_freq_calc(runmatch, loci_ls):
+    # Dictionaries of counts and frequencies
     overallcount = defaultdict(dict)
     overallfreq = defaultdict(dict)
     onemmcount = defaultdict(dict)
@@ -30,7 +32,7 @@ def aamm_freq_calc(runmatch, loci_ls):
                 mm_count = (mm1_count + mm2_count) / len(aa_pos)
 
             if mm1_count == 0:
-                mm1_freq = 0
+                mm1_freq = 0  # division by zero gives an error so have to do if statements
             else:
                 mm1_freq = mm1_count / len(aa_pos)
             if mm2_count == 0:
@@ -74,28 +76,53 @@ def counts_freqs(count_dict, freq_dict, which_count):
     return count_freq_df
 
 
-oldmatch_filename = './kidney-outcomes-sfvt/out.runmatchMC.1.txt.gz'  # Found in /kidney-outcomes-sfvt
-newmatch_filename = './srtr-impute-pubsaf2306/out.runmatchMC.1.txt.gz'  # Found in /srtr-impute-pubsaf2306
+oldmatch_filename = './kidney-outcomes-sfvt/out.runmatchMC.1.txt.gz'  # Found in /lustre/project/lgragert/kidney-outcomes-sfvt
+# newmatch_filename = './srtr-impute-pubsaf2306/out.runmatchMC.1.txt.gz' # '/lustre/project/lgragert/srtr-impute-pubsaf2306/out.runmatchMC.1.txt.gz'
 
 old_runmatch = pd.read_csv(oldmatch_filename, sep='|', compression='gzip', header=None, names=['PXID', 'Locus', 'Position', '0MM', '1MM', '2MM'])
-new_runmatch = pd.read_csv(newmatch_filename, sep='|', compression='gzip', header=None, names=['PXID', 'Locus', 'Position', '0MM', '1MM', '2MM'])
+# new_runmatch = pd.read_csv(newmatch_filename, sep='|', compression='gzip', header=None, names=['PXID', 'Locus', 'Position', '0MM', '1MM', '2MM'])
 
-new_loci = ['A', 'C', 'B', 'DRB345', 'DRB1', 'DQA1', 'DQB1', 'DPA1', 'DPB1']
+# new_loci = ['A', 'C', 'B', 'DRB345', 'DRB1', 'DQA1', 'DQB1', 'DPA1', 'DPB1']
 old_loci = ['A', 'C', 'B', 'DRB1', 'DQB1']
 
 # Use the def to get the MM frequency calculations for the old and new data
 oldcount, oldfreq, oldcount1, oldfreq1, oldcount2, oldfreq2 = aamm_freq_calc(old_runmatch, old_loci)
-newcount, newfreq, newcount1, newfreq1, newcount2, newfreq2 = aamm_freq_calc(new_runmatch, new_loci)
+del old_runmatch
+# newcount, newfreq, newcount1, newfreq1, newcount2, newfreq2 = aamm_freq_calc(new_runmatch, new_loci)
+# del new_runmatch  # This clears up the memory, you no longer need this DataFrame
+
 
 # Turn the dictionary into a DataFrame with the frequency and counts in the same one
 old_freqmm = counts_freqs(oldcount, oldfreq, 'Overall')
 old_freq1mm = counts_freqs(oldcount1, oldfreq1, '1MM')
 old_freq2mm = counts_freqs(oldcount2, oldfreq2, '2MM')
 
-new_freqmm = counts_freqs(newcount, newfreq, 'Overall')
-new_freq1mm = counts_freqs(newcount1, newfreq1, '1MM')
-new_freq2mm = counts_freqs(newcount2, newfreq2, '2MM')
+# new_freqmm = counts_freqs(newcount, newfreq, 'Overall')
+# new_freq1mm = counts_freqs(newcount1, newfreq1, '1MM')
+# new_freq2mm = counts_freqs(newcount2, newfreq2, '2MM')
 
 # Concat all the MM counts and put into a CSV
 old_freq = pd.concat([old_freqmm, old_freq1mm['1MM_Count'], old_freq1mm['1MM_Frequency'], old_freq2mm['2MM_Count'], old_freq2mm['2MM_Frequency']], axis=1)
+# new_freq = pd.concat([new_freqmm, new_freq1mm['1MM_Count'], new_freq1mm['1MM_Frequency'], new_freq2mm['2MM_Count'], new_freq2mm['2MM_Frequency']], axis=1)
 old_freq.to_csv('old_AAMM_frequency.csv', index=False, header=True)
+# new_freq.to_csv('new_AAMM_frequency.csv', index=False, header=True)
+
+old_freq = pd.read_csv('old_AAMM_frequency.csv', header=0)
+new_freq = pd.read_csv('new_AAMM_frequency.csv', header=0)
+
+new_freq['MM_Freq_PUBSAF2306'] = new_freq['Overall_Frequency']
+old_freq['MM_Freq_PUBSAF1812'] = old_freq['Overall_Frequency']
+
+new_freq = new_freq[['Locus', 'Position', 'MM_Freq_PUBSAF2306']]
+old_freq = old_freq[['Locus', 'Position', 'MM_Freq_PUBSAF1812']]
+
+# Filter to only old_loci for new_freq
+new_old_freq = pd.merge(new_freq, old_freq, how='inner', on=['Locus', 'Position'])
+new_old_freq['Abs_Diff'] = new_old_freq['MM_Freq_PUBSAF2306'] - new_old_freq['MM_Freq_PUBSAF1812']
+new_old_freq.to_csv('pubsaf_absdiff_old_loci.csv', index=False, header=True)
+
+# Keep all rows contained to show big difference in analysis
+old_new_freq = pd.merge(new_freq, old_freq, how='outer', on=['Locus', 'Position'])
+old_new_freq = old_new_freq.fillna(0)
+old_new_freq['Abs_Diff'] = old_new_freq['MM_Freq_PUBSAF2306'] - old_new_freq['MM_Freq_PUBSAF1812']
+old_new_freq.to_csv('pubsaf_absdiff_new_loci.csv', index=False, header=True)
