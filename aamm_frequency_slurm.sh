22885912b05c5ecc0c8a11c6456fb01e0b012d76
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-04-10 09:26:13 -0500

    Slurm script for aamm_frequency.py

---
+++ aamm_frequency_slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=AAMM_freq   ### Job Name
+#SBATCH --output=./AAMM_freq.out       ### File in which to store job output
+#SBATCH --error=./AAMM_freq.err        ### File in which to store job error messages
+#SBATCH --qos=long          ### Quality of Service (like a queue in PBS)
+#SBATCH --partition=centos7   ### Required for Python 3.11
+#SBATCH --mem=256000          ### Memory required because of the DataFrame
+#SBATCH --time=0-168:00:00     ### Wall clock time limit in Days-HH:MM:SS
+#SBATCH --nodes=1             ### Node count required for the job
+#SBATCH --ntasks-per-node=1   ### Number of tasks to be launched per Node
+#SBATCH --mail-type=END
+#SBATCH --mail-user=apaynter@tulane.edu
+
+module load anaconda3/2023.07
+python3 AAMM_frequency.py 
