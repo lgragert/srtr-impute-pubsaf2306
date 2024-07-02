ccf428f3a17670de6a1537dc8735001598dbc605
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-07-02 09:58:49 -0500

    Run serially instead of array
    Pyard locks due to concurrency issues.

--- aa_mm_biopython_runmatch_genie_9loc_RUN_I2C2.sh
+++ aa_mm_biopython_runmatch_genie_9loc_RUN_I2C2.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
-#BSUB -J AAMM_RM[1-20]                  ### Job Name with array specficied
-#BSUB -oo /project/kamoun_shared/apaynter/logs/AAMM_RM_%I.out         ### File in which to store job output
-#BSUB -eo /project/kamoun_shared/apaynter/logs/AAMM_RM_%I.err         ### File in which to store job error messages
-#BSUB -q i2c2_normal                    ### Quality of Service
-#BSUB -M 128000                         ### Memory limit per-process (KB)
-#BSUB -n 1                              ### sets the core
+#BSUB -J AAMM_RM                 ### Job Name with array specficied
+#BSUB -o ./logs/%J.out         ### File in which to store job output
+#BSUB -e ./logs/%J.err         ### File in which to store job error messages
+#BSUB -q i2c2_long                    ### Quality of Service
+#BSUB -M 128000                         ### Memory limit per-process
 #BSUB -u apaynter@tulane.edu            ### send me mail
 #BSUB -N
-
-replicate=$(($((${LSB_JOBINDEX})) % 10))
-if [ $replicate -eq 0 ]; then
-  replicate=10
-fi
-
-runmatch=$(($(($((${LSB_JOBINDEX})) - 1)) / 10))
-matrix=1
-if [ $runmatch -eq 1 ]; then
-  matrix=0
-fi
-sfvt=0
-
-echo ${LSB_JOBINDEX} $replicate $runmatch $matrix $sfvt
-
-SECONDS=0
 
 source activate /project/kamoun_shared/apaynter/conda-envs/apR
 cd /project/kamoun_shared/code_shared/srtr-impute-pubsaf2306
 
-echo "Environment set - Executing Python script"
-
-python aa_mm_biopython_runmatch_genie_9loc.py $replicate $runmatch $matrix $sfvt
+python aa_mm_biopython_runmatch_genie_9loc.py 1 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 2 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 3 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 4 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 5 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 6 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 7 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 8 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 9 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 10 1 0 0
 
 ELAPSED="Elapsed US: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
