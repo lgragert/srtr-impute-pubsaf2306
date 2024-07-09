41285c2e08ee6ca261c5023db0bf4b80f9f2dd1c
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-07-09 10:00:43 -0500

    i2c2_long queue does not exist. normal works for long jobs

--- aa_mm_biopython_runmatch_genie_9loc_RUN_I2C2.sh
+++ aa_mm_biopython_runmatch_genie_9loc_RUN_I2C2.sh
@@ -2,7 +2,7 @@
 #BSUB -J AAMM_RM                 ### Job Name with array specficied
 #BSUB -o ./logs/%J.out         ### File in which to store job output
 #BSUB -e ./logs/%J.err         ### File in which to store job error messages
-#BSUB -q i2c2_long                    ### Quality of Service
+#BSUB -q i2c2_normal                    ### Quality of Service
 #BSUB -M 128000                         ### Memory limit per-process
 #BSUB -u apaynter@tulane.edu            ### send me mail
 #BSUB -N
@@ -11,15 +11,26 @@ source activate /project/kamoun_shared/apaynter/conda-envs/apR
 cd /project/kamoun_shared/code_shared/srtr-impute-pubsaf2306
 
 python aa_mm_biopython_runmatch_genie_9loc.py 1 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 1 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 2 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 2 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 3 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 3 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 4 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 4 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 5 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 5 0 1 0
+
 python aa_mm_biopython_runmatch_genie_9loc.py 6 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 6 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 7 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 7 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 8 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 8 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 9 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 9 0 1 0
 python aa_mm_biopython_runmatch_genie_9loc.py 10 1 0 0
+python aa_mm_biopython_runmatch_genie_9loc.py 10 0 1 0
 
 ELAPSED="Elapsed US: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
