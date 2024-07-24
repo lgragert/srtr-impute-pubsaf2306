6014561db3a51529015540040b6db6d473c627b8
JK4800 <143016183+JK4800@users.noreply.github.com>
2024-07-24 16:22:48 -0500

    Increase wall clock time

--- hla_epitope_registry_eplet_test_RUN.sh
+++ hla_epitope_registry_eplet_test_RUN.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=hla_epitope_registry_eplet_test                                        ### Job Name
 #SBATCH --output=/lustre/project/lgragert/jk/logs/hla_epitope_registry_eplet_test.out     ### File in which to store job output (based on array task)
 #SBATCH --error=/lustre/project/lgragert/jk/logs/hla_epitope_registry_eplet_test.err      ### File in which to store job error messages (based on array task)
-#SBATCH --partition=centos7                                                               ### Partition (default is 'defq')
+#SBATCH --partition=centos7                                                               ### Partition required for Python 3.11 (default is 'defq')
 #SBATCH --qos=long                                                                        ### Quality of Service (like a queue in PBS)
-#SBATCH --mem=16000                                                                       ### Memory to request
-#SBATCH --time=0-48:00:00                                                                 ### Wall clock time limit in Days-HH:MM:SS
+#SBATCH --mem=256000                                                                      ### Memory required because of the DataFrame
+#SBATCH --time=0-168:00:00                                                                ### Wall clock time limit in Days-HH:MM:SS
 #SBATCH --nodes=1                                                                         ### Node count required for the job
 #SBATCH --ntasks-per-node=1                                                               ### Number of tasks to be launched per Node (MPI processes)
 #SBATCH --cpus-per-task=1                                                                 ### Number of threads per task (OMP threads)
