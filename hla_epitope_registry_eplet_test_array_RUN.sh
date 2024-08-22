341d4690d866d996966e50337209df51a95d591c
JK4800 <143016183+JK4800@users.noreply.github.com>
2024-08-22 11:30:00 -0500

    slurm script for srtr eplet array job

---
+++ hla_epitope_registry_eplet_test_array_RUN.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=hla_epitope_registry_eplet_test_array                 ### Job Name
+#SBATCH --array=1-10                                                     ### Number of array tasks
+#SBATCH --output=./logs/hla_epitope_registry_eplet_test_array_%A_%a.out  ### File in which to store job output (based on array task)
+#SBATCH --error=./logs/hla_epitope_registry_eplet_test_array_%A_%a.err   ### File in which to store job error messages (based on array task)
+#SBATCH --partition=centos7                                              ### Partition required for Python 3.11 (default is 'defq')
+#SBATCH --qos=long                                                       ### Quality of Service (like a queue in PBS)
+#SBATCH --mem=256000                                                     ### Memory required because of the DataFrame
+#SBATCH --time=0-168:00:00                                               ### Wall clock time limit in Days-HH:MM:SS
+
+
+
+
+#SBATCH --mail-type=ALL                                                  ### Mails the user the BEGIN, END, FAIL, REQUEUE, or ALL
+#SBATCH --mail-user=jko2@tulane.edu                                      ### tulaneID@tulane.edu
+
+module load anaconda3/2023.07
+cd /lustre/project/lgragert/jk/srtr-impute-pubsaf2306/
+
+echo "Running task with SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"       ### SLURM_ARRAY_TASK_ID refers to array number
+
+python3 hla_epitope_registry_eplet_test_array.py
